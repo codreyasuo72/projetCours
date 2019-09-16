e861fa07b134c7b0af003e78d7de7d4dba253d09
Codreyasuo72 <cody.foulon@etud.univ-angers.fr>
2019-09-16 11:43:17 +0200

    commentaire

---
+++ mylib.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#define true 1
+#define false 0
+
+typedef int bool ;
+
+bool anagramme(char *s1,char *s3);
+void palindrome(char mot[]);
